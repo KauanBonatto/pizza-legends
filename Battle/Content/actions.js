96949169c511f2dbef8ced2cb39a8761337d5a39
Kauan Bonatto <kauanbonatto01@gmail.com>
2024-09-28 23:11:05 -0300

    feature: Creating battles

---
+++ Battle/Content/actions.js
@@ -0,0 +1,10 @@
+window.Actions = {
+  damage1: {
+    name: "Whomp!",
+    success: [
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
+      // { type: "animation", animation: "willBeDefinedHere" },
+      { type: "stateChange", damage: 10 }
+    ]
+  }
+};
