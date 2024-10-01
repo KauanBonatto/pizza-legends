2b0f6ee867b8f78b122e814d1ac7fce59bf9f120
Kauan Bonatto <kauanbonatto01@gmail.com>
2024-09-30 23:09:58 -0300

    feature: Battle, building attack menu

--- Battle/SubmissionMenu.js
+++ Battle/SubmissionMenu.js
@@ -5,14 +5,84 @@ class SubmissionMenu {
     this.onComplete = onComplete;
   }
 
-  decide() {
+  getPages() {
+    const backOption = {
+      label: "Go Back",
+      description: "Return to previous page",
+      handler: () => {
+        this.keyboardMenu.setOptions(this.getPages().root);
+      }
+    };
+
+    return {
+      root: [
+        {
+          label: "Attack",
+          description: "Choose an attack",
+          handler: () => {
+            this.keyboardMenu.setOptions(this.getPages().attacks);
+          }
+        },
+        {
+          label: "Items",
+          description: "Choose an item",
+          disabled: true,
+          handler: () => {
+            this.keyboardMenu.setOptions(this.getPages().items);
+          }
+        },
+        {
+          label: "Swap",
+          description: "Change to another pizza",
+          handler: () => {
+            console.log("GO TO SWAP PAGE");
+          }
+        }
+      ],
+      attacks: [
+        ...this.caster.actions.map(key => {
+          const action = Actions[key];
+          return {
+            label: action.name,
+            description: action.description,
+            handler: () => {
+              this.menuSubmit(action);
+            }
+          }
+        }),
+        backOption
+      ],
+      items: [
+        backOption
+      ]
+    }
+  }
+
+  menuSubmit(action, instanceId = null) {
+    this.keyboardMenu?.end();
+
     this.onComplete({
-      action: Actions[this.caster.actions[0]],
-      target: this.enemy
+      action,
+      target: action.targetType === "friendly" ? this.caster : this.enemy
     });
   }
 
+  decide() {
+    this.menuSubmit(Actions[this.caster.actions[0]])
+  }
+
+  showMenu(container) {
+    this.keyboardMenu = new KeyboardMenu();
+    this.keyboardMenu.init(container);
+    this.keyboardMenu.setOptions(this.getPages().root);
+  }
+
   init(container) {
-    this.decide();
+
+    if (this.caster.isPlayerControlled) {
+      this.showMenu(container);
+    } else {
+      this.decide();
+    }
   }
 }
